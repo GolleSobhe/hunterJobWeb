7c29f6fbafd1735579e0df7e4f5c729a0385e55c
ly-abdourahamane <abdourahamane.ly1@gmail.com>
2018-05-07 14:37:23 +0200

    affichage des drapeaux des pays choisi

--- src/app/accueil/accueil/accueil.component.ts
+++ src/app/accueil/accueil/accueil.component.ts
@@ -8,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class AccueilComponent implements OnInit {
 
   tousLesPays;
-    
-  
+
+  states = [
+    {
+      name: 'Guinée',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Flag_of_Guinea.svg'
+    },
+    {
+      name: 'France',
+      flag: 'https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg'
+    },
+    {
+      name: 'Sénégal',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Flag_of_Senegal.svg'
+    },
+    {
+      name: 'Rwanda',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/1/17/Flag_of_Rwanda.svg'
+    },
+    {
+      name: 'Ghana',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/1/19/Flag_of_Ghana.svg'
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
@@ -19,7 +42,7 @@ export class AccueilComponent implements OnInit {
   private initialiserTousLesPays(): void{
     this.tousLesPays = [{codePays: "225", nomPays: "Cote d'ivoire"},
     {codePays: "33", nomPays: "France"},
-    {codePays: "224", nomPays: "Guinee"},    
+    {codePays: "224", nomPays: "Guinee"},
     {codePays: "245", nomPays: "Guinee-Bissau"},
     {codePays: "221", nomPays: "Senegal"}];
   }
