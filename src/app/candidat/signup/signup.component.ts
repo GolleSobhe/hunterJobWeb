e462622d1d714f6a9befd9aa68e5c36c2d25f7e5
Abdourahamane LY <niebores@gmail.com>
2019-03-16 13:24:30 +0100

    Add password validation

--- src/app/candidat/signup/signup.component.ts
+++ src/app/candidat/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { CandidatService } from '../candidat.service';
+import { MustMatch } from './match.password.validator';
 
 @Component({
   selector: 'comp-signup',
@@ -21,16 +22,18 @@ export class SignupComponent implements OnInit {
     this.userForm = this.formBuilder.group({
       displayName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      isRecruiter: [false, [Validators.nullValidator]]
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
     });
   }
 
   signUp() {
     if (this.userForm.valid) {
       let user = this.userForm.value;
+      //TODO: delete 'confirmPassword' and create user with service
       console.log(user);
-
       this.userForm.reset();
     }
   }
